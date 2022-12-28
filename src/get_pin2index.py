101171c4604351cc896f50ca77e6faddc090af25
Ruizhe Zhong <zhongruizhe271828@foxmail.com>
2022-12-28 08:54:39 +0800

    update

--- src/get_pin2index.py
+++ src/get_pin2index.py
@@ -5,9 +5,9 @@ def get_pin2index(cells, pipos):
     '''
     Index dict for pin.
     Given a pin, get its index.
-    pin is like '_347_.A1', where '_347_' is the instance name of cell, 'A1' is the pin.
+    pin is like '_347_{CELL_PIN_SEP}A1', where '_347_' is the instance name of cell, 'A1' is the pin.
     '''
-    print("Constructing mapping between cell.pin to index")
+    print(f"Constructing mapping between cell{CELL_PIN_SEP}pin to index")
     pins = []
     pins.extend(pipos['PI'])
     pins.extend(pipos['PO'])
